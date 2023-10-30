83979f11da3c0b0f25ff2eabd33a796b61c9c5ed
wavyxra <edsondanielbros@gmail.com>
2023-10-30 01:29:23 -0600

    no fue mucho pedo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return(
         <>
             <h1 className="text-center font-black text-5xl md:w-2/3 mx-auto">
-                Seguimiento Pacientes {""}  
+                Seguimiento Pacientes {""}  con Github {""}
                 <span className="text-indigo-600">Veterinaria</span>
             </h1>
         </>
